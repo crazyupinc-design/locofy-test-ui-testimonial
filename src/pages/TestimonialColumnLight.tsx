1ab2263cbeb98a3bd2e7e9147f90e15b4a0630a0
디자이너 <crazyupdesigner@dijaineoui-iMac.local>
2023-05-18 11:18:17 +0900

    https://www.figma.com/file/Y2GVmBMCVG1KJjWvVlkqqs/locofy-study?type=design&node-id=2-427

--- src/pages/TestimonialColumnLight.tsx
+++ src/pages/TestimonialColumnLight.tsx
@@ -3,62 +3,76 @@ import styles from "./TestimonialColumnLight.module.css";
 const TestimonialColumnLight: FunctionComponent = () => {
   return (
     <div className={styles.testimonialColumnLight}>
-      <b className={styles.title}>Testimonial section</b>
-      <div className={styles.columns}>
-        <div className={styles.column}>
-          <div className={styles.content}>
-            <div className={styles.quote}>
-              <div className={styles.text}>
-                “Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat.”
-              </div>
-              <div className={styles.author}>
-                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
-                <div className={styles.info}>
-                  <b className={styles.text}>Author Name</b>
-                  <div className={styles.vpOfCompany}>VP of Company</div>
+      <div className={styles.titleParent}>
+        <b className={styles.title}>Testimonial section</b>
+        <article className={styles.columns}>
+          <div className={styles.column}>
+            <div className={styles.content}>
+              <div className={styles.quote}>
+                <div className={styles.text}>
+                  “Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                  do amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat.”
+                </div>
+                <div className={styles.author}>
+                  <img
+                    className={styles.imageIcon}
+                    alt=""
+                    src="/image@2x.png"
+                  />
+                  <div className={styles.info}>
+                    <b className={styles.text}>Author Name</b>
+                    <div className={styles.vpOfCompany}>VP of Company</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.column}>
-          <div className={styles.content}>
-            <div className={styles.quote}>
-              <div className={styles.text}>
-                “Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat.”
-              </div>
-              <div className={styles.author}>
-                <img className={styles.imageIcon} alt="" src="/image1@2x.png" />
-                <div className={styles.info}>
-                  <b className={styles.text}>Author Name</b>
-                  <div className={styles.vpOfCompany}>VP of Company</div>
+          <div className={styles.column}>
+            <div className={styles.content}>
+              <div className={styles.quote}>
+                <div className={styles.text}>
+                  “Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                  do amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat.”
+                </div>
+                <div className={styles.author}>
+                  <img
+                    className={styles.imageIcon}
+                    alt=""
+                    src="/image1@2x.png"
+                  />
+                  <div className={styles.info}>
+                    <b className={styles.text}>Author Name</b>
+                    <div className={styles.vpOfCompany}>VP of Company</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.column}>
-          <div className={styles.content}>
-            <div className={styles.quote}>
-              <div className={styles.text}>
-                “Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat.”
-              </div>
-              <div className={styles.author}>
-                <img className={styles.imageIcon} alt="" src="/image2@2x.png" />
-                <div className={styles.info}>
-                  <b className={styles.text}>Author Name</b>
-                  <div className={styles.vpOfCompany}>VP of Company</div>
+          <div className={styles.column}>
+            <div className={styles.content}>
+              <div className={styles.quote}>
+                <div className={styles.text}>
+                  “Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                  do amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat.”
+                </div>
+                <div className={styles.author}>
+                  <img
+                    className={styles.imageIcon}
+                    alt=""
+                    src="/image2@2x.png"
+                  />
+                  <div className={styles.info}>
+                    <b className={styles.text}>Author Name</b>
+                    <div className={styles.vpOfCompany}>VP of Company</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
   );
